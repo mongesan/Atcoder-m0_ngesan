f7f56c3df448b5bee42f6e052d4759de7d1a0ef1
mongesan <syun1.mongesan@gmail.com>
2020-04-28 08:01:09 +0900

    auto-commited

--- Beginner-Contest/ABC095/ABC095_C.py
+++ Beginner-Contest/ABC095/ABC095_C.py
@@ -1,1 +1,16 @@
-#ABC095_C
+a, b, c, x, y = map(int, input().split())
+cost = int()
+tmp = min(x, y)
+x -= tmp
+y -= tmp
+if a+b > c*2:
+    cost += tmp*2*c
+else:
+    cost += tmp*(a+b)
+
+if x == 0:
+    cost += min(b, c*2)*y
+else:
+    cost += min(a, c*2)*x
+
+print(cost)
--- Beginner-Contest/ABC128/ABC128_C.py
+++ Beginner-Contest/ABC128/ABC128_C.py
@@ -1,1 +1,5 @@
-#ABC128_C
+n, m=map(int, input().split())
+s = list()
+k = s[0]
+del s[0]
+ 
