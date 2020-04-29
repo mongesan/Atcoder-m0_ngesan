8c230303a6492e31b8d34285d73ada2df32e2c25
mongesan <syun1.mongesan@gmail.com>
2020-04-30 08:58:13 +0900

    auto-commited

--- Beginner-Contest/ABC145/ABC145_C.py
+++ Beginner-Contest/ABC145/ABC145_C.py
@@ -1,1 +1,12 @@
-#ABC145_C
+from itertools import permutations
+from math import sqrt
+N = int(input())
+pos = list()
+for _ in range(N):
+    pos.append(tuple(map(int, input().split())))
+
+dist = list()
+for i in range(1,N):
+    dist.append(sqrt((pos[i-1][0]-pos[i][0])**2+(pos[i-1][1]-pos[i][1])**2))
+
+print(sum(dist)/len(dist))
--- Beginner-Contest/ABC150/ABC150_C.py
+++ Beginner-Contest/ABC150/ABC150_C.py
@@ -1,14 +1,10 @@
-#from math import factorial
-n = int(input())
-p = list(map(int, input().split()))
-q = list(map(int, input().split()))
-l = set()
+from itertools import permutations
+N = int(input())
+P = tuple(map(int, input().split()))
+Q = tuple(map(int, input().split()))
+per = list(permutations(range(1, N+1)))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        for k in range(1, n+1):
-            l.add((i, j, k))
-l = list(l)
-l.sort()
-print(l)
-print(l.index(tuple(p))-l.index(tuple(q)))
+p = per.index(P)
+q = per.index(Q)
+
+print(abs(p-q))
