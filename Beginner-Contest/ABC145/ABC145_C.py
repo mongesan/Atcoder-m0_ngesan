3f1a40d29015266c57bcf6e8a3030324cbf5ed48
mongesan <syun1.mongesan@gmail.com>
2020-05-01 08:15:20 +0900

    auto-commited

--- Beginner-Contest/ABC145/ABC145_C.py
+++ Beginner-Contest/ABC145/ABC145_C.py
@@ -2,11 +2,17 @@ from itertools import permutations
 from math import sqrt
 N = int(input())
 pos = list()
+r = list()
 for _ in range(N):
     pos.append(tuple(map(int, input().split())))
+for v in permutations(range(N), N):
+    r.append(v)
 
+print(r)
 dist = list()
-for i in range(1,N):
-    dist.append(sqrt((pos[i-1][0]-pos[i][0])**2+(pos[i-1][1]-pos[i][1])**2))
+for i in range(len(r)-1):
+    s
+    dist.append(sqrt((pos[i][0]-pos[i+1][0]) **
+                         2+(pos[i][1]-pos[i+1][1])**2))
 
 print(sum(dist)/len(dist))
