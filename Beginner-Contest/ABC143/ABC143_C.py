d5c8304586142ac22900c4a4fdcf9919fd71eaf1
mongesan <syun1.mongesan@gmail.com>
2020-05-02 07:49:00 +0900

    auto-commited

--- Beginner-Contest/ABC143/ABC143_C.py
+++ Beginner-Contest/ABC143/ABC143_C.py
@@ -1,1 +1,10 @@
-#ABC143_C
+n = int(input())
+s = str(input())
+t = str()
+cnt = int()
+for ch in s:
+    if t[-1:] != ch:
+        t += ch
+        cnt += 1
+
+print(cnt)
--- Beginner-Contest/ABC145/ABC145_C.py
+++ Beginner-Contest/ABC145/ABC145_C.py
@@ -1,18 +1,22 @@
-from itertools import permutations
 from math import sqrt
-N = int(input())
-pos = list()
-r = list()
-for _ in range(N):
-    pos.append(tuple(map(int, input().split())))
-for v in permutations(range(N), N):
-    r.append(v)
 
-print(r)
-dist = list()
-for i in range(len(r)-1):
-    s
-    dist.append(sqrt((pos[i][0]-pos[i+1][0]) **
-                         2+(pos[i][1]-pos[i+1][1])**2))
 
-print(sum(dist)/len(dist))
+def route(a, b):
+    return(sqrt((p[a-1][0]-p[b-1][0])**2+(p[a-1][1]-p[b-1][1])**2))
+
+
+n = int(input())
+p = list()
+for i in range(n):
+    p.append(tuple(map(int, input().split())))
+
+sum = int()
+cnt = int()
+for i in range(1, n):
+    for j in range(i+1, n+1):
+        #print(i, j)
+        sum += route(i, j)
+        cnt += 1
+
+sum *= n-1
+print(sum/cnt)
