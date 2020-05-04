c15f3878999ddf1cd82746bda792a0eca29f4194
mongesan <syun1.mongesan@gmail.com>
2020-05-05 08:15:39 +0900

    auto-commited

--- Beginner-Contest/ABC056/ABC056_C.py
+++ Beginner-Contest/ABC056/ABC056_C.py
@@ -1,1 +1,6 @@
-#ABC056_C
+from math import factorial
+X = int(input())
+for i in range(1, 10**5):
+    if X <= (i*(i+1))//2:
+        print(i)
+        break
--- Beginner-Contest/ABC116/ABC116_C.py
+++ Beginner-Contest/ABC116/ABC116_C.py
@@ -1,1 +1,17 @@
-#ABC116_C
+N=int(input())
+h=list(map(int, input().split()))
+cnt=0
+sp=0
+while sum(h)!=0:
+    for i in range(N):
+        if h[i]==0:
+            if i-sp!=0:
+                cnt+=1
+            sp=i+1
+            print(h)
+        else:
+            h[i] -= 1
+    if i-sp+1!=0:
+        cnt+=1
+    print(h)
+print(cnt)
