eec74cf0b1f001f1c803f802c24b73080da43264
mongesan <syun1.mongesan@gmail.com>
2020-05-06 07:52:36 +0900

    auto-commited

--- Beginner-Contest/ABC100/ABC100_C.py
+++ Beginner-Contest/ABC100/ABC100_C.py
@@ -1,1 +1,16 @@
-#ABC100_C
+N = int(input())
+a = list(map(int, input().split()))
+
+cnt = 0
+ans = 0
+while cnt!=3:
+    for n in range(N):
+        if a[n]%2!=0 or (N!=1 and n):
+            cnt+=1
+            a[n] *= 3
+        else:
+            a[n] //= 2
+    if cnt!=3:
+        ans += 1
+
+print(ans)
--- Beginner-Contest/ABC103/ABC103_C.py
+++ Beginner-Contest/ABC103/ABC103_C.py
@@ -1,1 +1,13 @@
-#ABC103_C
+N=int(input())
+a=list(map(int, input().split()))
+
+ans = 0
+m = 1
+for i in range(N):
+    m *= a[i]
+
+m-=1
+for A in a:
+    ans += m % A
+
+print(ans)
