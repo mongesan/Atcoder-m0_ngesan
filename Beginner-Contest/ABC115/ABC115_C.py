9d159670028b16dd0c51232a5f5f72ecd8cb6a47
mongesan <syun1.mongesan@gmail.com>
2020-05-07 08:06:37 +0900

    auto-commited

--- Beginner-Contest/ABC115/ABC115_C.py
+++ Beginner-Contest/ABC115/ABC115_C.py
@@ -1,1 +1,10 @@
-#ABC115_C
+N, K = map(int, input().split())
+h = list()
+for i in range(N):
+    h.append(int(input()))
+h.sort()
+ans = 10**9+1
+for i in range(N-1):
+    ans = min(ans, abs(h[i]-h[i+1]))
+
+print(ans)
