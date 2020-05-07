fea829f69e9468ac60fd1c11d7b571186a48ac6d
mongesan <syun1.mongesan@gmail.com>
2020-05-08 08:27:59 +0900

    auto-commited

--- Beginner-Contest/ABC093/ABC093_C.py
+++ Beginner-Contest/ABC093/ABC093_C.py
@@ -1,1 +1,14 @@
-#ABC093_C
+l=list(map(int, input().split()))
+l.sort()
+cnt=0
+while l[0]!=l[1] or l[0]!=l[2] or l[1]!=l[2]:
+    l.sort()
+    #print(l)
+    if l[1] == l[2]:
+        l[0] += 2
+    else:
+        l[0] += 1
+        l[1] += 1
+    cnt+=1
+
+print(cnt)
--- Beginner-Contest/ABC115/ABC115_C.py
+++ Beginner-Contest/ABC115/ABC115_C.py
@@ -4,7 +4,7 @@ for i in range(N):
     h.append(int(input()))
 h.sort()
 ans = 10**9+1
-for i in range(N-1):
-    ans = min(ans, abs(h[i]-h[i+1]))
+for i in range(N-K+1):
+    ans = min(ans, abs(h[i]-h[i+K-1]))
 
 print(ans)
