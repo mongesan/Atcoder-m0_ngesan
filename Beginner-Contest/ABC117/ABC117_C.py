bb8721affbf363d8e19abc5c6844fe931942d8dd
mongesan <syun1.mongesan@gmail.com>
2020-05-14 08:32:16 +0900

    auto-commited

--- Beginner-Contest/ABC117/ABC117_C.py
+++ Beginner-Contest/ABC117/ABC117_C.py
@@ -1,1 +1,16 @@
-#ABC117_C
+N, M = map(int, input().split())
+X = list(map(int, input().split()))
+X.sort()
+
+if N >= M:
+    print(0)
+else:
+    sub = [0]*(M-1)
+    for i in range(M-1):
+        sub[i] = X[i+1]-X[i]
+    sub.sort()
+
+    ans = 0
+    for i in range(M-N):
+        ans += sub[i]
+    print(ans)
