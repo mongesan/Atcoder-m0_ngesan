68c97a3ae7bfe9a21ebd0e77206d396138a932f9
mongesan <syun1.mongesan@gmail.com>
2020-06-08 08:30:48 +0900

    auto-commited

--- Beginner-Contest/ABC129/ABC129_C.py
+++ Beginner-Contest/ABC129/ABC129_C.py
@@ -1,1 +1,15 @@
-#ABC129_C
+n, m=map(int, input().split())
+A = list()
+F = list()
+for i in range(m):
+    A.append(int(input()))
+x = 0
+for i in range(1,n+1):
+    x+=i
+    F.append(n)
+F.append(n)
+sp = 0
+x = 0
+while n>sp
+:
+
