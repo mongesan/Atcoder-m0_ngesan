e1f9bdab65f0763c4b67885d2f785ab048aed3fd
mongesan <syun1.mongesan@gmail.com>
2020-06-11 08:31:22 +0900

    auto-commited

--- Beginner-Contest/ABC129/ABC129_C.py
+++ Beginner-Contest/ABC129/ABC129_C.py
@@ -1,15 +1,20 @@
 n, m=map(int, input().split())
 A = list()
-F = list()
+Fib = [1,1,1]
 for i in range(m):
     A.append(int(input()))
 x = 0
-for i in range(1,n+1):
-    x+=i
-    F.append(n)
-F.append(n)
+for i in range(3,n+1):
+    Fib.append(Fib[i-1]+Fib[i-2])
+print(Fib)
 sp = 0
-x = 0
-while n>sp
-:
-
+cnt = 1
+for M in A:
+    cnt*=Fib[M-sp]
+    print(cnt)
+    if M-sp == 1:
+        if M!=1:
+            cnt *= 0
+    sp=M
+cnt*=Fib[n-sp]
+print(cnt % 1000000007)
