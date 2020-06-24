40ead53591ab7e58ac837569d9020400e67b9dc0
mongesan <syun1.mongesan@gmail.com>
2020-06-25 07:29:10 +0900

    auto-commited

--- Beginner-Contest/ABC006/ABC006_B.py
+++ Beginner-Contest/ABC006/ABC006_B.py
@@ -1,1 +1,8 @@
-#ABC006_B
+n=int(input())
+tri=[0,0,1]
+for i in range(n-3):
+    ans = (tri[i]+tri[i+1]+tri[i+2])%10007
+    tri.append(ans)
+if n==1 or n==2 or n==3:
+    ans = tri[n-1]
+print(ans)
--- Beginner-Contest/ABC018/ABC018_B.py
+++ Beginner-Contest/ABC018/ABC018_B.py
@@ -1,1 +1,7 @@
-#ABC018_B
+s = str(input())
+n = int(input())
+for i in range(n):
+    # print(s)
+    l, r = map(int, input().split())
+    s = s[:l-1]+s[l-1:r][::-1]+s[r:]
+print(s)
