973dae751e94436c53f9164cc3657d12d6326bee
mongesan <syun1.mongesan@gmail.com>
2020-06-29 20:18:59 +0900

    auto-commited

--- Regular-Contest/ARC089/ARC089_A.py
+++ Regular-Contest/ARC089/ARC089_A.py
@@ -1,1 +1,11 @@
-#ARC089_A
+n = int(input())
+X, Y, T=0, 0, 0
+for i in range(n):
+    t, x, y = map(int, input().split())
+    if abs(x-X)+abs(y-Y) <= t-T and t % 2 == (x+y) % 2:
+        continue
+    else:
+        print('No')
+        quit()
+    X, Y, T= x, y, T
+print('Yes')
