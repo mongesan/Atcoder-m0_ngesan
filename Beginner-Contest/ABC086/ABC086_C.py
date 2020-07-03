6c40845572b196de133365d7f1db8a9c52926985
mongesan <syun1.mongesan@gmail.com>
2020-07-03 12:35:04 +0900

    0703

--- Beginner-Contest/ABC086/ABC086_C.py
+++ Beginner-Contest/ABC086/ABC086_C.py
@@ -1,1 +1,13 @@
-n=int(input())
+n = int(input())
+tb,xb,yb = 0,0,0
+for _ in range(n):
+    ta,xa,ya = list(map(int, input().split()))
+    tdiff = abs(ta-tb)
+    mdiff = abs(xa-xb) + abs(ya-yb)
+    if tdiff < mdiff or (tdiff + mdiff) & 1:
+        print('No')
+        exit()
+    
+    tb,xb,yb = ta,xa,ya
+
+print('Yes')
--- Beginner-Contest/ABC088/ABC088_C.py
+++ Beginner-Contest/ABC088/ABC088_C.py
@@ -1,1 +1,13 @@
-#ABC088_C
+c = [list(map(int, input().split())) for i in range(3)]
+x = c[0][2]-c[0][1]
+y = c[0][1]-c[0][0]
+for i in range(1, 3):
+    if c[i][1]-c[i][0] != y:
+        print('No')
+        break
+    if c[i][2]-c[i][1] != x:
+        print('No')
+        break
+
+else:
+    print('Yes')
