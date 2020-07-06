7e4e602227fbffa06273acd43210ccabdcca0c3d
mongesan <syun1.mongesan@gmail.com>
2020-07-06 22:29:42 +0900

    0706-2

--- Beginner-Contest/ABC058/ABC058_C.py
+++ Beginner-Contest/ABC058/ABC058_C.py
@@ -1,1 +1,17 @@
-#ABC058_C
+from collections import Counter
+N = int(input())
+tmp = list(input())
+da = Counter(tmp)
+for _ in range(N-1):
+    tmp = list(input())
+    tmpd = Counter(tmp)
+    for k, v in da.items():
+        if not tmpd[k]:
+            da[k] = 0
+        else:
+            da[k] = min(v, tmpd[k])
+ans = ""
+for k, v in da.items():
+    ans += str(k)*v
+a = "".join(sorted(ans))
+print(a)
