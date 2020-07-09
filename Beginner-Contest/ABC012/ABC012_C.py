5d69d53b5527cf200048b965321397ea42820cb0
mongesan <syun1.mongesan@gmail.com>
2020-07-09 09:16:39 +0900

    0709

--- Beginner-Contest/ABC012/ABC012_C.py
+++ Beginner-Contest/ABC012/ABC012_C.py
@@ -1,1 +1,9 @@
-#ABC012_C
+n = int(input())
+m = int()
+for i in range(1, 10):
+    m += i*45
+n = m-n
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i*j == n:
+            print("%d x %d" % (i, j))
--- Beginner-Contest/ABC028/ABC028_C.py
+++ Beginner-Contest/ABC028/ABC028_C.py
@@ -1,1 +1,8 @@
-#ABC028_C
+from itertools import combinations
+l=list(map(int, input().split()))
+c=list(combinations(l, 3))
+n=list()
+for x in c:
+    n.append(sum(x))
+
+print(sorted(n)[len(n)-3])
