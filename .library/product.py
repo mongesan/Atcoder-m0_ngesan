8054c774ea8dbb5abb0b70f735f0c9d14354962c
mongesan <syun1.mongesan@gmail.com>
2020-07-22 08:54:27 +0900

    0722

---
+++ .library/product.py
@@ -0,0 +1,3 @@
+from itertools import product
+list(product(range(3), repeat=2))
+# -> [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
--- Beginner-Contest/ABC029/ABC029_C.py
+++ Beginner-Contest/ABC029/ABC029_C.py
@@ -1,1 +1,5 @@
-#ABC029_C
+from itertools import product
+n = int(input())
+d = ['a', 'b', 'c']
+for s in list(product(d, repeat=n)):
+    print(''.join(s))
