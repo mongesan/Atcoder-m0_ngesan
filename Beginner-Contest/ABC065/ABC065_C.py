93f86a872b768dbc8451060aa72a17cb1fa78067
mongesan <syun1.mongesan@gmail.com>
2020-07-25 20:54:11 +0900

    0725

--- Beginner-Contest/ABC065/ABC065_C.py
+++ Beginner-Contest/ABC065/ABC065_C.py
@@ -1,1 +1,9 @@
-#ABC065_C
+from math import factorial
+n,m=map(int, input().split())
+ans=0
+if abs(n-m)==0:
+    ans=factorial(n)*factorial(m)*2
+elif abs(n-m)==1:
+    ans=factorial(n)*factorial(m)
+
+print(ans%(10**9+7))
--- Beginner-Contest/ABC078/ABC078_C.py
+++ Beginner-Contest/ABC078/ABC078_C.py
@@ -1,1 +1,2 @@
-n,m=map(int, input().split())
+N, M = map(int, input().split())
+print((1900*M+100*(N-M))*2**M)
