14bc585a8f2a716618777861478c80cf1212fdef
mongesan <syun1.mongesan@gmail.com>
2020-07-26 10:20:45 +0900

    0726

--- Beginner-Contest/ABC003/ABC003_C.py
+++ Beginner-Contest/ABC003/ABC003_C.py
@@ -1,1 +1,6 @@
-#ABC003_C
+n, k = map(int, input().split())
+R = sorted(list(map(int, input().split())))
+n = 0
+for r in R[n-k:]:
+    n = (n+r)/2
+print(n)
--- Beginner-Contest/ABC005/ABC005_C.py
+++ Beginner-Contest/ABC005/ABC005_C.py
@@ -1,1 +1,20 @@
-#ABC005_C
+t = int(input())
+n = int(input())
+A = list(map(int, input().split()))
+m = int(input())
+B = list(map(int, input().split()))
+box = []
+
+for b in B:
+    # print(A)
+    for i in range(n):
+        # print(A[i]+t,b,A[i],b)
+        if A[i]+t >= b and A[i] <= b:
+            del A[i]
+            n -= 1
+            break
+    else:
+        print('no')
+        exit()
+
+print('yes')
