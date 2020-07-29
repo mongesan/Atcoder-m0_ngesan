3fc93e7ef0b0d54d69161433001f89cd11b5706e
mongesan <syun1.mongesan@gmail.com>
2020-07-30 08:35:53 +0900

    0729

--- Beginner-Contest/ABC061/ABC061_C.py
+++ Beginner-Contest/ABC061/ABC061_C.py
@@ -1,1 +1,11 @@
-#ABC061_C
+n,k=map(int, input().split())
+l=[tuple(map(int, input().split())) for _ in range(n)]
+l.sort()
+a,b=0,0
+for t in l:
+    b=a+t[1]
+    if a<k<=b:
+        print(t[0])
+        exit()
+    a=b
+print(l)
