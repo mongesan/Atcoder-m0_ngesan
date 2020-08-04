b24312702c155354c02f148b345e1c87d1abdff0
mongesan <syun1.mongesan@gmail.com>
2020-08-05 08:32:41 +0900

    0805

--- Beginner-Contest/ABC072/ABC072_D.py
+++ Beginner-Contest/ABC072/ABC072_D.py
@@ -1,1 +1,11 @@
-#ABC072_D
+n=int(input())
+P=list(map(int, input().split()))
+ans=0
+n=1
+for i in P:
+    print(i,n)
+    if i==n:
+        ans+=1
+    n+=1
+print(ans)
+print(ans//2+ans%2)
--- Beginner-Contest/ABC131/ABC131_D.py
+++ Beginner-Contest/ABC131/ABC131_D.py
@@ -1,1 +1,11 @@
-#ABC131_D
+n=int(input())
+l=[tuple(map(int, input().split())) for i in range(n)]
+l.sort(key=lambda x:x[1])
+cnt=0
+for t in l:
+    cnt+=t[0]
+    if cnt>t[1]:
+        print('No')
+        exit()
+
+print('Yes')
