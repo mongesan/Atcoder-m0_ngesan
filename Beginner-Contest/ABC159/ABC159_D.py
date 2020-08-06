c3406da5f608bf7dc3c74517ee7f84e8af07e420
mongesan <syun1.mongesan@gmail.com>
2020-08-06 15:09:14 +0900

    0806

--- Beginner-Contest/ABC159/ABC159_D.py
+++ Beginner-Contest/ABC159/ABC159_D.py
@@ -1,1 +1,21 @@
-#ABC159_D
+from math import factorial
+from collections import Counter
+
+
+def combinations_count(n, r):
+    if n >= r:
+        return factorial(n) // (factorial(n - r) * factorial(r))
+    else:
+        return 0
+
+c = Counter()
+n = int(input())
+A = list(map(int, input().split()))
+for i in A:
+    c[i] += 1
+ans = 0
+for k, v in c.items():
+    ans += combinations_count(v, 2)
+
+for i in A:
+    print(ans-c[i]+1)
