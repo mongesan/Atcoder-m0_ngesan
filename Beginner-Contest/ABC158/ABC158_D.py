2c1ff1d9fb874aaaeb845b1e7ff7b73ea6693b6b
mongesan <syun1.mongesan@gmail.com>
2020-08-15 20:59:06 +0900

    start of abc175

--- Beginner-Contest/ABC158/ABC158_D.py
+++ Beginner-Contest/ABC158/ABC158_D.py
@@ -1,17 +1,13 @@
 s = str(input())
 q = int(input())
 for i in range(q):
-    query = list(map(str, input().split()))
-    query[0]=int(query[0])
-    if len(query)!=1:
-        query[1]=int(query[1])
-    if query[0] == 1:
-        if len(s) != 1:
-            s = s[len(s)-1]+s[1:len(s)-1]+s[0]
-    else:
-        if query[1] == 1:
-            s = query[2] + s
+    tmp = tuple(map(str, input().split()))
+    if len(tmp) == 3:  # pattern 2
+        if int(tmp[1]) == 1:
+            s = tmp[2]+s
         else:
-            s += query[2]
+            s += tmp[2]
+    else:
+        s = s[::-1]
 
 print(s)
