e18cd4088c584437a96b3aa0ac8e58edec0b5c08
mongesan <syun1.mongesan@gmail.com>
2020-09-14 16:23:42 +0900

    unko

--- Beginner-Contest/ABC129/ABC129_C.py
+++ Beginner-Contest/ABC129/ABC129_C.py
@@ -1,26 +1,16 @@
-n, m=map(int, input().split())
-A = [int(input()) for i in range(m)]
-Fib = [0,1,2]
-for i in range(2,10**5-2):
-    Fib.append(Fib[i-1]+Fib[i])
-way=1
-i=1
-j=0
-cnt=1
-l=[]
-A.append(n)
-while i<=n:
-    if i==A[j]:
-        l.append(cnt-1)
-        print(cnt-1)
-        cnt=0
-        j+=1
-    i+=1
-    cnt+=1
-if l[0]==0:
-    del l[0]
-
-for i in l:
-    way*=Fib[i+1]
-
-print(way%1000000007)
+n,m=map(int, input().split())
+Fib=[1,2]
+x=1
+while Fib[x]+Fib[x-1]<=n:
+    Fib.append(Fib[x]+Fib[x-1])
+    x+=1
+p=0
+m=1
+for _ in range(m):
+    a=int(input())
+    if a!=1 and a-p==1:
+        m*=0
+    else:
+        m*=Fib[a-p]
+    p=a
+print(n-p)
--- Beginner-Contest/ABC162/ABC162_D.py
+++ Beginner-Contest/ABC162/ABC162_D.py
@@ -1,1 +1,30 @@
-#ABC162_D
+n = int(input())
+S = str(input())
+r = []
+g = []
+b = []
+for i in range(n):
+    if S[i] == 'R':
+        r.append(i)
+    elif S[i] == 'G':
+        g.append(i)
+    else:
+        b.append(i)
+#print(r, g, b)
+ans = 0
+for R in r:
+    for G in g:
+        i = min(R, G)
+        j = max(R, G)
+        for B in b:
+            if j < B:
+                if j-i != B-j:
+                    ans += 1
+            elif B < i:
+                if j-i != i-B:
+                    ans += 1
+            else:
+                if j-B != B-i:
+                    ans += 1
+
+print(ans)
