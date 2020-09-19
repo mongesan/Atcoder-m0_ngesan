9c236fd9d0b4526b5c8db5a72907d2bcc61fe000
mongesan <syun1.mongesan@gmail.com>
2020-09-19 20:42:52 +0900

    ABC179

--- Beginner-Contest/ABC158/ABC158_D.py
+++ Beginner-Contest/ABC158/ABC158_D.py
@@ -1,13 +1,31 @@
-s=str(input())
-q=int(input())
-Q=[]
-r=0
+from collections import deque
+s = str(input())
+q = int(input())
+stat = 0
+l = []
+a = deque()
+b = deque()
 for i in range(q):
-    t,f,c=map(int, input().split())
-    if t==1:
-        if r==0:
-            r+=1
-        else:
-            r-=1
+    l = list(map(str, input().split()))
+    t = int(l[0])
+    if t == 1:
+        stat += 1
     else:
-        Q.append((f,c))
+        f = int(l[1])
+        c = l[2]
+        if stat % 2 == 0:
+            if f == 1:
+                a.appendleft(c)
+            else:
+                b.append(c)
+        else:
+            if f == 1:
+                b.append(c)
+            else:
+                a.appendleft(c)
+
+#print(a, b, stat % 2)
+ans = ''.join(a)+s+''.join(b)
+if stat % 2 == 1:
+    ans = ans[::-1]
+print(ans)
